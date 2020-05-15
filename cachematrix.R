7bf5b5bf76ba56bac2a1bc139ae49db95362abdc
RobMader <r.maderitsch@gmail.com>
2020-05-15 18:16:49 +0200

    Get

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function is adjusted to matrix instead of vector and inverse is taken instead of mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
+## This function is now adjusted to matrix instead of vector and solve instead of mean
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
+
+# Test
+a <- makeCacheMatrix(matrix(1:4,2,2))
+a$get()
+a$getInverse()
+cacheSolve(a)
+cacheSolve(a)
